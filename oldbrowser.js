bed767354d8254cdf24be97c794c2a770b04a5cf
Manuel Gonzalez <manuel2788@gmail.com>
2014-12-17 16:00:36 -0430

    first commit

---
+++ oldbrowser.js
@@ -0,0 +1,53 @@
+// get current browser
+//
+function getBrowser () {
+	var browser = navigator.userAgent;
+	return browser.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i);
+}
+
+// check if current browser is IE
+// 
+// @param name (string)
+//
+function isIE(name) {
+	return name == "MSIE" ? true : false;
+}
+
+// check if version of IE <= 8
+// 
+// @param version (number)
+//
+function isIE8(version) {
+	return version <= 8 ? true : false; 
+}
+
+// Old browser construct
+// 
+// @param browser (array)
+//
+var OldBrowser = function (browser) {
+	this.name = browser[1];
+	this.version = Number(browser[2]);
+};
+
+OldBrowser.prototype.get = function() {
+	return this.name + " en la versión " + this.version;
+};
+
+OldBrowser.prototype.checkIE = function() {
+	var output;
+
+	if (!isIE(this.name) && !isIE8(this.version)) {
+		output = "No damos soporte a la version (" + this.version + ") de Internet Explorer.";
+	}
+
+	return output;
+};
+
+OldBrowser.prototype.print = function() {
+	if (!isIE(this.name) && !isIE8(this.version)) {
+		var wrapped = document.getElementById("detect-browser");
+		wrapped.className += " active";
+		document.getElementById("title").innerHTML = currentBrowser.checkIE();
+	}
+};
